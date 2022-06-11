32b6b34437d8dc5066c11cb3b620cb8f7c80bfb7
Ashirbad Panigrahi <ashirbadapanigrahi@gmail.com>
2022-06-11 10:25:37 +0530

    users controller refactored with mongoose

--- src/controllers/usersController.ts
+++ src/controllers/usersController.ts
@@ -1,19 +1,35 @@
 import { Request, Response } from "express";
+import { model, Schema } from "mongoose";
 import { User } from "../types";
 
-let users: User[] = [];
+const userSchema = new Schema<User>(
+  {
+    displayName: { type: String, required: true },
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+  },
+  { collection: "USERS" }
+);
+const UsersModel = model<User>("User", userSchema);
 
 const getAllUsers = async (req: Request, res: Response) => {
-  res.status(200).json({
-    data: { users },
-    message: "Users fetched successfully",
-  });
+  try {
+    const users = await UsersModel.find({});
+    res.status(200).json({
+      data: { users },
+      message: "Users fetched successfully",
+    });
+  } catch (error) {
+    res.status(500).json({
+      data: { error },
+    });
+  }
 };
 
 const createUser = async (req: Request, res: Response) => {
   try {
-    const user: User = req.body;
-    users.push(user);
+    console.log(req.body);
+    const user = await UsersModel.create(req.body);
     res.status(200).json({
       data: { user },
       message: "User created successfully",
@@ -28,13 +44,12 @@ const createUser = async (req: Request, res: Response) => {
 
 const updateUser = async (req: Request, res: Response) => {
   try {
-    const updatedUserData: User = req.body;
-    const updatedUsers = users.map((user) => {
-      if (user._id === updatedUserData._id)
-        return { ...user, ...updatedUserData };
-      return user;
-    });
-    users = updatedUsers;
+    const updatedUser = req.body;
+    const updatedUserData = await UsersModel.findByIdAndUpdate(
+      updatedUser._id,
+      updatedUser,
+      { new: true }
+    );
     res.status(200).json({
       data: { updatedUserData },
       message: "User updated successfully",
@@ -49,11 +64,9 @@ const updateUser = async (req: Request, res: Response) => {
 
 const deleteUser = async (req: Request, res: Response) => {
   try {
-    const deletedUserId = req.params._id;
-    const updatedUsers = users.filter((user) => user._id !== deletedUserId);
-    users = updatedUsers;
+    const deletedUser = await UsersModel.findByIdAndDelete(req.params._id);
     res.status(200).json({
-      data: { deletedUserId },
+      data: { deletedUserId: deletedUser?._id, deletedUser },
       message: "User deleted successfully",
     });
   } catch (error) {
