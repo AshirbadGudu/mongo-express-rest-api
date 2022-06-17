85c349bbeaa188ec77b1d0c2b6a61395c3a998ed
Ashirbad Panigrahi <ashirbadapanigrahi@gmail.com>
2022-06-17 08:20:30 +0530

    CategoryModel created

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,6 +1,7 @@
 import { model } from "mongoose";
-import { ProductSchema, UserSchema } from "../schemas";
-import { Product, User } from "../types";
+import { CategorySchema, ProductSchema, UserSchema } from "../schemas";
+import { Category, Product, User } from "../types";
 
 export const UsersModel = model<User>("User", UserSchema);
 export const ProductModel = model<Product>("Product", ProductSchema);
+export const CategoryModel = model<Category>("Category", CategorySchema);
