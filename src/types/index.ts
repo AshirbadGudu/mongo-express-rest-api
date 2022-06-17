1a1639fec0cdff2bda4cbd8a672f7709cf88a7f5
Ashirbad Panigrahi <ashirbadapanigrahi@gmail.com>
2022-06-17 06:51:23 +0530

    Product type added

--- src/types/index.ts
+++ src/types/index.ts
@@ -4,3 +4,14 @@ export type User = {
   email: string;
   password: string;
 };
+
+export type Product = {
+  _id: string;
+  name: string;
+  price: number;
+  description: string;
+  image: string;
+  createdAt: Date;
+  updatedAt: Date;
+  category: string;
+};
