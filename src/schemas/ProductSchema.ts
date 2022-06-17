384e124216c478df52fd31aa61ad0972e44226e4
Ashirbad Panigrahi <ashirbadapanigrahi@gmail.com>
2022-06-17 06:53:36 +0530

    ProductSchema created

---
+++ src/schemas/ProductSchema.ts
@@ -0,0 +1,40 @@
+import { Schema } from "mongoose";
+import { Product } from "../types";
+
+export default new Schema<Product>(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: 3,
+      maxlength: 50,
+    },
+    price: {
+      type: Number,
+      required: true,
+      min: 1,
+    },
+    description: {
+      type: String,
+      minlength: 3,
+      maxlength: 1200,
+    },
+    image: {
+      type: String,
+    },
+    category: {
+      type: String,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      immutable: true,
+    },
+    updatedAt: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  { collection: "Products" }
+);
