feb1aa4ba9055ae1ec657c36b8b6b1b9aaa7f6ee
Ashirbad Panigrahi <ashirbadapanigrahi@gmail.com>
2022-06-17 06:53:59 +0530

    ProductModel created

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,5 +1,6 @@
 import { model } from "mongoose";
-import { UserSchema } from "../schemas";
-import { User } from "../types";
+import { ProductSchema, UserSchema } from "../schemas";
+import { Product, User } from "../types";
 
 export const UsersModel = model<User>("User", UserSchema);
+export const ProductModel = model<Product>("Product", ProductSchema);
